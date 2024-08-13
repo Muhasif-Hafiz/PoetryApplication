8fa0d1d878bf54299730943bd1092df2db21e359
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2024-08-13 23:21:58 +0530

    first commit-prepared UI

---
+++ app/src/main/java/DataEntry.kt
@@ -0,0 +1,3 @@
+import java.util.Date
+
+data class DataEntry(var id : Int, var date  : Date, var poem :String)
--- app/src/main/java/com/muhasib/mypoetry/MainActivity.kt
+++ app/src/main/java/com/muhasib/mypoetry/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.muhasib.mypoetry
 
+import DataEntry
+import RvAdapter
+import SampleData
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +13,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.muhasib.mypoetry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private var poemList = ArrayList<DataEntry>()
+    private val rvAdapter by lazy {
+        RvAdapter(this,poemList)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +32,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        poemList.addAll(SampleData.data)
+
+        binding.rv.layoutManager=LinearLayoutManager(this)
+        binding.rv.adapter=rvAdapter
+
 
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+            startActivity(Intent(this, AddEditActivity::class.java))
         }
     }
 
